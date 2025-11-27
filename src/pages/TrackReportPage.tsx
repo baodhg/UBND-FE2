540737f3e1dee20d6f2b3f50343a2607b5ea714f
Chau Hoang <chauhnmss181219@fpt.edu.vn>
2025-11-27 15:25:49 +0700

    Fix ui search report

--- src/pages/TrackReportPage.tsx
+++ src/pages/TrackReportPage.tsx
@@ -4,7 +4,7 @@ import { TrackReportForm } from './report-page/TrackReportForm'
 
 export const TrackReportPage: React.FC = () => {
   return (
-    <div className="py-6 sm:py-8 lg:py-12 bg-gray-50">
+    <div className="py-6 sm:py-8 lg:py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Card
           className="shadow-md"
